7a3aaa96a16c9600a90ded0b1fe8b86e65bcf503
Quoc Nguyen <quoc.nguyen@fractal.vn>
2021-03-16 23:24:21 +0700

    Change layout

--- app/src/main/java/com/example/audioplayerdemo/MainActivity.kt
+++ app/src/main/java/com/example/audioplayerdemo/MainActivity.kt
@@ -6,11 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         volumeControlStream = AudioManager.STREAM_MUSIC
+
     }
 }
