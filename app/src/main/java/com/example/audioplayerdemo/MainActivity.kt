5a26468f31b6629c635a6f4a4e37b6d134807fbb
Quoc Nguyen <boss.randomz@gmail.com>
2021-03-17 15:46:29 +0700

    Add music service connection

--- app/src/main/java/com/example/audioplayerdemo/MainActivity.kt
+++ app/src/main/java/com/example/audioplayerdemo/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.audioplayerdemo
 
 import android.media.AudioManager
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
+import android.support.v4.media.session.MediaControllerCompat
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +15,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         volumeControlStream = AudioManager.STREAM_MUSIC
+//        val musicPath = Uri.parse("android.resource://" + packageName + "/" + R.raw.music)
+//        mediaPlayer.setDataSource(applicationContext, musicPath)
+//        sb_music.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+//            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+//                if (fromUser) mediaPlayer.seekTo(progress)
+//            }
+//
+//            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+//
+//            }
+//
+//            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+//
+//            }
+//
+//        })
 
     }
 }
